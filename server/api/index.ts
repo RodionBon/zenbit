81eed540d5a51451572db8a92b69ec0f3121c271
Rodion <rodionb4649@gmail.com>
2024-10-03 15:24:42 +0300

    Update index.ts

--- server/api/index.ts
+++ server/api/index.ts
@@ -4,13 +4,11 @@ import { AppModule } from './app.module';
 const origins = ['https://zenbit-ten.vercel.app'];
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: { origin: origins },
-  });
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: origins,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // 必要に応じて
+    credentials: true,
   });
   await app.listen(3000);
 }
