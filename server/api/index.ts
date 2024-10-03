15ac5ec411bddfdc245938eaea6115c056b7b757
Rodion <rodionb4649@gmail.com>
2024-10-03 15:27:01 +0300

    Update index.ts

--- server/api/index.ts
+++ server/api/index.ts
@@ -5,11 +5,7 @@ const origins = ['https://zenbit-ten.vercel.app'];
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: origins,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
