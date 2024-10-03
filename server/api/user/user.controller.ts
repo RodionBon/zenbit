b2fbee00dcebecba6583dc98b447636f41f191ce
Rodion <rodionb4649@gmail.com>
2024-10-03 14:40:23 +0300

    Update user.controller.ts

--- server/api/user/user.controller.ts
+++ server/api/user/user.controller.ts
@@ -10,11 +10,32 @@ import {
 import * as jwt from 'jsonwebtoken';
 import * as bcrypt from 'bcrypt';
 import 'dotenv/config';
-import signInDto from 'api/user/dto/sign-in-dto';
-import signUpDto from 'api/user/dto/sign-up-dto';
-import signOutDto from 'api/user/dto/sign-out-dto';
 import supabase from 'api/database';
 import 'dotenv/config';
+
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+class signOutDto {
+  @IsEmail()
+  email: string;
+}
+
+class signInDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+}
+class signUpDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+}
 
 @Controller('user')
 export class LoginController {
@@ -47,114 +68,114 @@ export class LoginController {
     return { email: signUpDto.email };
   }
 
-  // @Get('get-user')
-  // async getUser(@Headers() headers: Record<string, string>) {
-  //   const token = headers.authorization?.split(' ')[1];
-  //   if (!token) {
-  //     throw new HttpException({ message: 'No token' }, HttpStatus.FORBIDDEN);
-  //   }
-  //   // const { data: userData } = await supabase
-  //   //   .from('user')
-  //   //   .select('id')
-  //   //   .eq('email', signOutDto.email);
+  @Get('get-user')
+  async getUser(@Headers() headers: Record<string, string>) {
+    const token = headers.authorization?.split(' ')[1];
+    if (!token) {
+      throw new HttpException({ message: 'No token' }, HttpStatus.FORBIDDEN);
+    }
+    // const { data: userData } = await supabase
+    //   .from('user')
+    //   .select('id')
+    //   .eq('email', signOutDto.email);
 
-  //   // if (userData.length === 0) {
-  //   //   throw new HttpException(
-  //   //     { message: 'User is not found' },
-  //   //     HttpStatus.FORBIDDEN,
-  //   //   );
-  //   // }
+    // if (userData.length === 0) {
+    //   throw new HttpException(
+    //     { message: 'User is not found' },
+    //     HttpStatus.FORBIDDEN,
+    //   );
+    // }
 
-  //   const { data: sessionData } = await supabase
-  //     .from('session')
-  //     .select()
-  //     .eq('token', token);
+    const { data: sessionData } = await supabase
+      .from('session')
+      .select()
+      .eq('token', token);
 
-  //   const { data: userData } = await supabase
-  //     .from('user')
-  //     .select()
-  //     .eq('id', sessionData[0].user_id);
+    const { data: userData } = await supabase
+      .from('user')
+      .select()
+      .eq('id', sessionData[0].user_id);
 
-  //   return { user: userData[0] };
-  // }
+    return { user: userData[0] };
+  }
 
-  // @Post('sign-in')
-  // async signIn(@Body() signInDto: signInDto) {
-  //   const { data } = await supabase
-  //     .from('user')
-  //     .select('*')
-  //     .eq('email', signInDto.email);
+  @Post('sign-in')
+  async signIn(@Body() signInDto: signInDto) {
+    const { data } = await supabase
+      .from('user')
+      .select('*')
+      .eq('email', signInDto.email);
 
-  //   if (data.length === 0) {
-  //     throw new HttpException(
-  //       { message: 'User not found' },
-  //       HttpStatus.NOT_FOUND,
-  //     );
-  //   }
+    if (data.length === 0) {
+      throw new HttpException(
+        { message: 'User not found' },
+        HttpStatus.NOT_FOUND,
+      );
+    }
 
-  //   if (!(await bcrypt.compare(signInDto.password, data[0].password))) {
-  //     throw new HttpException(
-  //       { message: 'Password is incorrect' },
-  //       HttpStatus.FORBIDDEN,
-  //     );
-  //   }
+    if (!(await bcrypt.compare(signInDto.password, data[0].password))) {
+      throw new HttpException(
+        { message: 'Password is incorrect' },
+        HttpStatus.FORBIDDEN,
+      );
+    }
 
-  //   const token = this.generateJwtToken(signInDto.email);
+    const token = this.generateJwtToken(signInDto.email);
 
-  //   const response = await supabase.from('session').insert({
-  //     user_id: data[0]['id'],
-  //     token,
-  //   });
+    const response = await supabase.from('session').insert({
+      user_id: data[0]['id'],
+      token,
+    });
 
-  //   return { email: signInDto.email, token };
-  // }
+    return { email: signInDto.email, token };
+  }
 
-  // @Post('sign-out')
-  // async signOut(
-  //   @Body() signOutDto: signOutDto,
-  //   @Headers() headers: Record<string, string>,
-  // ) {
-  //   const token = headers.authorization?.split(' ')[1];
-  //   if (!token) {
-  //     throw new HttpException({ message: 'No token' }, HttpStatus.FORBIDDEN);
-  //   }
-  //   // const { data: userData } = await supabase
-  //   //   .from('user')
-  //   //   .select('id')
-  //   //   .eq('email', signOutDto.email);
+  @Post('sign-out')
+  async signOut(
+    @Body() signOutDto: signOutDto,
+    @Headers() headers: Record<string, string>,
+  ) {
+    const token = headers.authorization?.split(' ')[1];
+    if (!token) {
+      throw new HttpException({ message: 'No token' }, HttpStatus.FORBIDDEN);
+    }
+    // const { data: userData } = await supabase
+    //   .from('user')
+    //   .select('id')
+    //   .eq('email', signOutDto.email);
 
-  //   // if (userData.length === 0) {
-  //   //   throw new HttpException(
-  //   //     { message: 'User is not found' },
-  //   //     HttpStatus.FORBIDDEN,
-  //   //   );
-  //   // }
+    // if (userData.length === 0) {
+    //   throw new HttpException(
+    //     { message: 'User is not found' },
+    //     HttpStatus.FORBIDDEN,
+    //   );
+    // }
 
-  //   // const userId = userData[0].id;
+    // const userId = userData[0].id;
 
-  //   const { data: sessionData } = await supabase
-  //     .from('session')
-  //     .select('*')
-  //     .eq('token', token);
+    const { data: sessionData } = await supabase
+      .from('session')
+      .select('*')
+      .eq('token', token);
 
-  //   if (sessionData.length === 0) {
-  //     throw new HttpException(
-  //       { message: 'Session not found' },
-  //       HttpStatus.BAD_REQUEST,
-  //     );
-  //   }
+    if (sessionData.length === 0) {
+      throw new HttpException(
+        { message: 'Session not found' },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
 
-  //   if (sessionData[0]['token'] !== token) {
-  //     throw new HttpException({ message: 'Wrong token' }, HttpStatus.FORBIDDEN);
-  //   }
+    if (sessionData[0]['token'] !== token) {
+      throw new HttpException({ message: 'Wrong token' }, HttpStatus.FORBIDDEN);
+    }
 
-  //   return { sessionData };
-  // }
+    return { sessionData };
+  }
 
-  // generateJwtToken(email: string) {
-  //   const payload = { email };
-  //   const jwtSecret = process.env.JWT_SECRET;
-  //   const token = jwt.sign(payload, jwtSecret, { expiresIn: '1h' });
-  //   return token;
-  // }
+  generateJwtToken(email: string) {
+    const payload = { email };
+    const jwtSecret = process.env.JWT_SECRET;
+    const token = jwt.sign(payload, jwtSecret, { expiresIn: '1h' });
+    return token;
+  }
 }
