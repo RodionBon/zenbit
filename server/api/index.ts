2a4b7d9a6120c70ba051845e4b6aa37d78f92ce3
Rodion <rodionb4649@gmail.com>
2024-10-03 13:26:40 +0300

    123

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 const origins = ['http://localhost:5173'];
 
@@ -8,8 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: { origin: origins },
   });
-  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
- 
