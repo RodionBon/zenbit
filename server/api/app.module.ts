242841781ae3fb3b7b7e30bac83a551d4188d890
Rodion <rodionb4649@gmail.com>
2024-10-03 14:45:06 +0300

    Update app.module.ts

--- server/api/app.module.ts
+++ server/api/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoginModule } from './user/user.module';
-// import { DealModule } from './deal/deal.module';
+import { DealModule } from './deal/deal.module';
 
 @Module({
-  imports: [LoginModule],
+  imports: [LoginModule, DealModule],
   controllers: [AppController],
   providers: [AppService],
 })
