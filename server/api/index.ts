337d8c028a1396b4d4aaf6827ff8f35efbb01385
Rodion <rodionb4649@gmail.com>
2024-10-03 14:48:26 +0300

    Update index.ts

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const origins = ['http://localhost:5173'];
+const origins = ['http://localhost:5173', 'https://zenbit-ten.vercel.app'];
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
