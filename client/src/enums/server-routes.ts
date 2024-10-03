89c08f8a3a0dbc76cd953813d47d9546045c5624
Rodion <rodionb4649@gmail.com>
2024-10-03 14:52:42 +0300

    Update server-routes.ts

--- client/src/enums/server-routes.ts
+++ client/src/enums/server-routes.ts
@@ -1,5 +1,5 @@
 enum ServerRoutes {
-	SERVER_ADDRESS = "http://localhost:3000",
+	SERVER_ADDRESS = "https://zenbit-server.vercel.app",
 	SIGN_IN = "user/sign-in",
 	SIGN_UP = "user/sign-up",
 	SIGN_OUT = "user/sign-out",
