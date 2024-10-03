902d514180fba2460d29ed74e1d03aa819478741
Rodion <rodionb4649@gmail.com>
2024-10-03 14:04:14 +0300

    1234

--- server/api/app.module.ts
+++ server/api/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoginModule } from './user/user.module';
-import { DealModule } from './deal/deal.module';
+// import { DealModule } from './deal/deal.module';
 
 @Module({
   imports: [LoginModule],
--- server/api/deal/deal.controller.ts
+++ server/api/deal/deal.controller.ts
@@ -1,34 +1,34 @@
-import {
-  Body,
-  Controller,
-  Get,
-  HttpException,
-  HttpStatus,
-} from '@nestjs/common';
-import GetDealDto from './dto/get-deal-dto';
-import supabase from 'api/database';
+// import {
+//   Body,
+//   Controller,
+//   Get,
+//   HttpException,
+//   HttpStatus,
+// } from '@nestjs/common';
+// import GetDealDto from './dto/get-deal-dto';
+// import supabase from 'api/database';
 
-import { objectToCamel } from 'api/helpers/object-case';
+// import { objectToCamel } from 'api/helpers/object-case';
 
-@Controller('deal')
-export class DealController {
-  @Get('/')
-  async getDeal() {
-    const { data: dealData } = await supabase.from('deal').select('*');
+// @Controller('deal')
+// export class DealController {
+//   @Get('/')
+//   async getDeal() {
+//     const { data: dealData } = await supabase.from('deal').select('*');
 
-    const response = await Promise.all(
-      dealData.map(async (deal) => {
-        const filePath = await supabase.storage
-          .from('images')
-          .getPublicUrl(deal['file_path']).data.publicUrl;
+//     const response = await Promise.all(
+//       dealData.map(async (deal) => {
+//         const filePath = await supabase.storage
+//           .from('images')
+//           .getPublicUrl(deal['file_path']).data.publicUrl;
 
-        return {
-          ...objectToCamel(deal),
-          filePath,
-        };
-      }),
-    );
+//         return {
+//           ...objectToCamel(deal),
+//           filePath,
+//         };
+//       }),
+//     );
 
-    return response;
-  }
-}
+//     return response;
+//   }
+// }
